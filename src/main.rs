ed02ae900901d3892bc80bb1e32c8b63a2607ee5
TooFatToKidnapp <abdouali422@gmail.com>
2024-06-19 19:25:51 +0100

    setup a basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+
+#[derive(serde::Serialize)]
+struct ResponseMessage {
+    message: String
+}
+
+
+async fn handler() -> HttpResponse {
+    HttpResponse::Ok().json(ResponseMessage {
+        message: "Hello From Actix Server!!".to_string()
+    })
+}
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8000").expect(
+        "Failed to bind"
+    );
+    let _server = HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(handler))
+    })
+    .listen(listener)?
+    .run()
+    .await;
+
+    Ok(())
 }
