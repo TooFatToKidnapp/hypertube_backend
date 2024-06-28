ea66ca10ffeb61e340a789d80a733dca569c3ff9
Youssef Labtaimi <oyoucef099@gmail.com>
2024-06-28 15:21:43 +0100

    allowed wildcards for origins

--- src/startup.rs
+++ src/startup.rs
@@ -13,19 +13,31 @@ use actix_web::http::header;
 
 use std::env;
 
-pub fn run_server(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = Data::new(db_pool);
-    let server = HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin(
-                env::var("FRONTEND_URL")
-                    .expect("FRONTEND_URL must be set")
-                    .as_str(),
-            )
+fn configure_cors(frontend_url: &str) -> Cors {
+    if frontend_url == "*" {
+        Cors::default()
+            .allow_any_origin()
             .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE"])
             .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
             .allowed_header(header::CONTENT_TYPE)
-            .max_age(3600);
+            .max_age(3600)
+    } else {
+        Cors::default()
+            .allowed_origin(frontend_url)
+            .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600)
+    }
+}
+
+pub fn run_server(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
+    let frontend_url = env::var("FRONTEND_URL").expect("FRONTEND_URL must be set");
+
+    let server = HttpServer::new(move || {
+        let cors = configure_cors(&frontend_url);
+
         App::new()
             .wrap(cors)
             .wrap(TracingLogger::default())
