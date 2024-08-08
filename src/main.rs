5e202adacd295eb8182558611421eaafbc6056b8
TooFatToKidnapp <abdouali422@gmail.com>
2024-08-08 16:38:58 +0100

    added some error handling

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,21 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use std::process::{Command, Stdio};
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    check_for_necessary_env()?;
     let _output = Command::new("rqbit")
         .arg("server")
         .arg("start")
         .arg("/goinfre/aabdou/Download")
         .stdout(Stdio::null())
         .spawn()?;
-    dotenv().ok();
-    check_for_necessary_env();
     let subscriber = get_subscriber("hyper_tube", "info", std::io::stdout);
     init_subscriber(subscriber);
-    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind");
-    let configuration = get_configuration("configuration").expect(
-        "Failed to read `configuration.json`. Please make sure it exists and is valid JSON.",
-    );
+    let listener = TcpListener::bind("0.0.0.0:8000")?;
+    let configuration = get_configuration("configuration").map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to read `configuration.json`. Please make sure it exists and is valid JSON.")))?;
     let connection_pool = PgPool::connect(configuration.database.connection_string().as_str())
         .await
         .expect("Failed to connect to database");
--- src/util.rs
+++ src/util.rs
@@ -1,26 +1,27 @@
 use std::env;
 
-pub fn check_for_necessary_env() {
-    env::var("FRONTEND_URL").expect("FRONTEND_URL env must be set");
-    env::var("DATABASE_URL").expect("DATABASE_URL env must be set");
-    env::var("JWT_SECRET").expect("JWT_SECRET env must be set");
-    env::var("CLIENT_UID_42").expect("CLIENT_UID_42 env must be set");
-    env::var("CLIENT_SECRET_42").expect("CLIENT_SECRET_42 env must be set");
-    env::var("BACKEND_URL").expect("BACKEND_URL env must be set");
-    env::var("GOOGLE_CLIENT_ID").expect("GOOGLE_CLIENT_ID env must be set");
-    env::var("GOOGLE_CLIENT_SECRET").expect("GOOGLE_CLIENT_SECRET env must be set");
-    env::var("GOOGLE_CLIENT_SCOPE").expect("GOOGLE_CLIENT_SCOPE env must be set");
-    env::var("FAILURE_REDIRECT_URI").expect("FAILURE_REDIRECT_URI env must be set");
-    env::var("CLIENT_ID_GITHUB").expect("CLIENT_ID_GITHUB env must be set");
-    env::var("CLIENT_SECRET_GITHUB").expect("CLIENT_SECRET_GITHUB env must be set");
-    env::var("CLIENT_SCOPE_GITHUB").expect("CLIENT_SCOPE_GITHUB env must be set");
-    env::var("EMAIL_SENDER_USERNAME").expect("EMAIL_SENDER_USERNAME env must be set");
-    env::var("EMAIL_SENDER_PASSWORD").expect("EMAIL_SENDER_PASSWORD env must be set");
-    env::var("S3_BUCKET_SECRET_KEY").expect("S3_BUCKET_SECRET_KEY env must be set");
-    env::var("S3_BUCKET_ACCESS_KEY").expect("S3_BUCKET_ACCESS_KEY env must be set");
-    env::var("S3_BUCKET_NAME").expect("S3_BUCKET_NAME env must be set");
-    env::var("S3_PROVIDER_URL").expect("S3_PROVIDER_URL env must be set");
-    env::var("S3_BUCKET_URL").expect("S3_BUCKET_URL env must be set");
-    env::var("S3_REGION").expect("S3_REGION env must be set");
-    env::var("MOVIE_DB_AUTH_TOKEN").expect("MOVIE_DB_AUTH_TOKEN env must be set");
+pub fn check_for_necessary_env() -> Result<(), std::io::Error> {
+    env::var("FRONTEND_URL").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("FRONTEND_URL {}.", e)))?;
+    env::var("DATABASE_URL").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("DATABASE_URL {}.", e)))?;
+    env::var("JWT_SECRET").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("JWT_SECRET {}.", e)))?;
+    env::var("CLIENT_UID_42").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("CLIENT_UID_42 {}.", e)))?;
+    env::var("CLIENT_SECRET_42").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("CLIENT_SECRET_42 {}.", e)))?;
+    env::var("BACKEND_URL").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("BACKEND_URL {}.", e)))?;
+    env::var("GOOGLE_CLIENT_ID").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("GOOGLE_CLIENT_ID {}.", e)))?;
+    env::var("GOOGLE_CLIENT_SECRET").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("GOOGLE_CLIENT_SECRET {}.", e)))?;
+    env::var("GOOGLE_CLIENT_SCOPE").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("GOOGLE_CLIENT_SCOPE {}.", e)))?;
+    env::var("FAILURE_REDIRECT_URI").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("FAILURE_REDIRECT_URI {}.", e)))?;
+    env::var("CLIENT_ID_GITHUB").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("CLIENT_ID_GITHUB {}.", e)))?;
+    env::var("CLIENT_SECRET_GITHUB").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("CLIENT_SECRET_GITHUB {}.", e)))?;
+    env::var("CLIENT_SCOPE_GITHUB").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("CLIENT_SCOPE_GITHUB {}.", e)))?;
+    env::var("EMAIL_SENDER_USERNAME").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("EMAIL_SENDER_USERNAME {}.", e)))?;
+    env::var("EMAIL_SENDER_PASSWORD").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("EMAIL_SENDER_PASSWORD {}.", e)))?;
+    env::var("S3_BUCKET_SECRET_KEY").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_BUCKET_SECRET_KEY {}.", e)))?;
+    env::var("S3_BUCKET_ACCESS_KEY").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_BUCKET_ACCESS_KEY {}.", e)))?;
+    env::var("S3_BUCKET_NAME").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_BUCKET_NAME {}.", e)))?;
+    env::var("S3_PROVIDER_URL").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_PROVIDER_URL {}.", e)))?;
+    env::var("S3_BUCKET_URL").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_BUCKET_URL {}.", e)))?;
+    env::var("S3_REGION").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("S3_REGION {}.", e)))?;
+    env::var("MOVIE_DB_AUTH_TOKEN").map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("MOVIE_DB_AUTH_TOKEN {}.", e)))?;
+    Ok(())
 }
